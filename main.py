0a182ebde55debd1ecef336443bd453bbbb4c888
ronmabii <ronmabii@gmail.com>
2019-12-19 17:21:51 -0500

    a bit of praw progress

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import json
 import praw
-testing
+
+with open('credentials.json') as f:
+    params = json.load(f)
+
+reddit = praw.Reddit(client_id = params['client_id'],
+                     client_secret = params['client_secret'],
+                     username = params['username'],
+                     password = params['password'], 
+                     user_agent = ['user_agent']
+)
+
+subreddit = reddit.subreddit('manga')
+
+new_python = subreddit.new(limit=5)
+
+for submission in new_python:
+    print(submission.title)
