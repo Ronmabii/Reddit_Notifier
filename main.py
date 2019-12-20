4b2b11f5676f4f6963702ba4113012e7aeb6f12c
ronmabii <ronmabii@gmail.com>
2019-12-19 21:18:51 -0500

    some comments

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import json
 import praw
 
+# loads credentials from json file
 with open('credentials.json') as f:
     params = json.load(f)
 
-reddit = praw.Reddit(client_id = params['client_id'],
-                     client_secret = params['client_secret'],
-                     username = params['username'],
-                     password = params['password'], 
-                     user_agent = ['user_agent']
-)
+# logs into reddit
+reddit = praw.Reddit(client_id=params['client_id'],
+                     client_secret=params['client_secret'],
+                     username=params['username'],
+                     password=params['password'],
+                     user_agent=['user_agent'])
 
+# reddit calls
 subreddit = reddit.subreddit('manga')
 
 new_python = subreddit.new(limit=5)
 
 for submission in new_python:
     print(submission.title)
+
+
+# TODO pick manga/ connect to mangaplus /chart of upload dates/notify you dum
