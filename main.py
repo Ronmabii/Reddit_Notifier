7b128180a59b9d2e1e18580be8ce69d12402b237
ronmabii <ronmabii@gmail.com>
2019-12-27 19:16:31 -0500

    testing limits

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ reddit = praw.Reddit(client_id=params['client_id'],
 # reddit calls
 subreddit = reddit.subreddit('manga')
 
-new_python = subreddit.new(limit=5)
+new_python = subreddit.new(limit=985) # limit of 984(max?)(5ish days ago) works but slight stutter every 100 posts
 
+x=1
 for submission in new_python:
+    
     print(submission.title)
+    print(x)
+    x+=1
 
 # TODO priority: get links for notifier
 # TODO pick manga/ connect to mangaplus /chart of upload dates/notify you dum
 # TODO other: windows task manager to run on login
+# maybe get avg num of posts daily for predicting limit
