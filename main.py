0bd4af78d2675698e458da7d5a5ff815bab98b4d
ronmabii <ronmabii@gmail.com>
2020-01-06 20:47:19 -0500

    idea for credentials problem

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ reddit = praw.Reddit(client_id=params['client_id'],
                      password=params['password'],
                      user_agent=['user_agent'])
 
-# pick a subreddit
+# pick a subreddit (technically not necessary)
 subreddit = reddit.subreddit('manga')
 
 '''new_python = subreddit.new(limit=1)  # limit of 984(max?)(5ish days ago)(replaced by stream() but need a way to get old posts(or host online))
 (have a separate loop for old posts in the range of 12ish hours? EDIT : maybe not, stream had a 15 hour ago result) test'''
 x = 1
 
-#constant stream of submissions
+#constant stream of submissions (gets previous 100 posts up to current then updates every post)
 for submission in subreddit.stream.submissions():
     if any(_ in submission.title for _ in ["MangaDex", "[ART] Fubuki", "a Legend", "Black ",'What',"[DISC]", "This"]):  # case sensitive(might want lower method)(also use list(?) to do multi)
         print("---" + str(x) + "---")
@@ -44,4 +44,4 @@ print("HEYYYYYYYYYYYYYYYY")
 # TODO other: windows task manager to run on login
 # maybe get avg num of posts daily for predicting limit
 # stream never ends so we're stuck in that loop until the end of days maybe
-# possible host on heroku cuz its free
+# possible host on heroku cuz its free (Pro tip use heroku environmental variables to store credentials)
